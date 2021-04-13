889b2df8690c4eda17977ffe44cf44654744ac39
Sely Lychee <up949539@myport.ac.uk>
2021-04-13 20:43:27 +0100

    Adding the project codes

---
+++ app/src/main/java/com/uidesign/userexperience/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package com.uidesign.userexperience;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
